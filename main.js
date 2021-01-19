dfcbc0c8e80193c1906df126b84eb716ba688ae5
puge <mail@puge.org>
2021-01-20 00:58:15 +0800

    save

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function createWindow () {
   // mainWindow.loadFile('./test/index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
